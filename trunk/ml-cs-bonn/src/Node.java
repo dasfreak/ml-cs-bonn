490e2d7954bc5f32964d008a1200a027d29b1d0d
marc.ohm89@googlemail.com <marc.ohm89@googlemail.com@34124984-cd06-a1ee-f7bf-09feb149d9c3>
2014-11-02 14:58:19 +0000

    Basic binary tree (insert, printInOrder)

---
+++ trunk/ml-cs-bonn/src/Node.java
@@ -0,0 +1,9 @@
+class Node {
+	Node left;
+	Node right;
+	int value;
+
+	public Node(int value) {
+		this.value = value;
+	}
+}
--- trunk/ml-cs-bonn/src/test.java
+++
@@ -1,9 +0,0 @@
-// test 2
-public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Test");
-	}
-
-}
