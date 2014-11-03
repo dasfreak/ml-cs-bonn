1fc4f44e1e5aa343babff1f8aef8d286ce6a6a63
marc.ohm89@googlemail.com <marc.ohm89@googlemail.com@34124984-cd06-a1ee-f7bf-09feb149d9c3>
2014-11-03 13:53:12 +0000

    * added Dataset * added loadDatasetFromFile function

--- trunk/ml-cs-bonn/src/Train.java
+++ trunk/ml-cs-bonn/src/Train.java
@@ -1,7 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 
 public class Train {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		String[][] dataset = loadDatasetFromFile("data_exercise_1.csv");
+		
+		System.out.println("Line 0: "+Arrays.toString(dataset[0]));
+		System.out.println("Line 100: "+Arrays.toString(dataset[100]));
+
 		Node root = new Node(5);
 		insert(root, 1);
 		insert(root, 8);
@@ -9,6 +21,37 @@ public class Train {
 		insert(root, 3);
 		insert(root, 9);
 		printInOrder(root);
+	}
+
+	public static String[][] loadDatasetFromFile(String Filename) throws FileNotFoundException, IOException {
+
+		String fileInput = "";
+
+		// read file
+		try(BufferedReader br = new BufferedReader(new FileReader(Filename))) {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(";");
+				line = br.readLine();
+			}
+			fileInput = sb.toString();
+		}
+
+		// split at end of line
+		String[] splitResult = fileInput.split(";");
+
+		// split every feature 
+		String[][] dataset = new String[splitResult.length][];
+
+		for (int x=0; x<splitResult.length; x++){
+			dataset[x] = splitResult[x].split(",");
+		}
+
+		return dataset;
+
 	}
 
 
