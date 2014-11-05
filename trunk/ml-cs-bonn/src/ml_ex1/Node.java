b8689416a7dec87e5e0336aa86356998cc97d892
elias3@gmail.com <elias3@gmail.com@34124984-cd06-a1ee-f7bf-09feb149d9c3>
2014-11-05 21:13:54 +0000

    -algorithm to create the tree implemented (still buggy)

--- trunk/ml-cs-bonn/src/ml_ex1/Node.java
+++ trunk/ml-cs-bonn/src/ml_ex1/Node.java
@@ -8,67 +8,59 @@ public class Node {
 	public String name; //name of the node read from file
 	public int index; //column of the node in file
 	public double entropy;
-	public double numRecords;
+	public int numRecords;
 	public double informationGain;
 	public Attribute attribute; //attribute of the node
-	
+
 	public ArrayList<Node> children;
     public Node parent=null;
-    public List<Segment> segments; 
+	Subset maximal = null;
+    public List<Subset> subsets; 
     
 	
 	//do not know yet if class_name is necessary here, we will see later on
-	public Node(int index, Attribute attribute, String nodeName){
-		segments=new ArrayList<Segment>();
-		this.name=nodeName;
-		this.attribute=attribute;
-		this.index=index; //column in our data set
+	public Node(int index, Attribute attribute, String nodeName, Node parent){
+		subsets        = new ArrayList<Subset>();
+		this.name      = nodeName;
+		this.attribute = attribute;
+		this.index     = index; //column in our data set
+		this.parent    = parent;
+		this.children  = new ArrayList<Node>();
 	}
 	
 	
 	/* extracts segments for certain data column and saves this segment into segments list */
-	public void extractSegments(){
-		for (int i=0; i<DataStructureSingleton.getInstance1().length ; i++){
-			Segment temp=new Segment(DataStructureSingleton.getInstance1()[i][index]);
-			
-			if (segments.isEmpty()){
-				segments.add(temp);
-				if ((boolean) DataStructureSingleton.getInstance1()[0][DataStructureSingleton.getInstance1()[0].length-1].getData())
-					segments.get(0).yes_number=1;
-				else
-					segments.get(0).no_number=1;
-			}
-			else{
-				int found=0;
-				for(int j=0; j<segments.size(); j++){
-					if (segments.get(j).attr.getData().equals(temp.attr.getData())){
-						found++;
-						segments.get(j).numer_of_occurrences++;
-						if ((boolean) DataStructureSingleton.getInstance1()[i][DataStructureSingleton.getInstance1()[0].length-1].getData())
-							segments.get(j).yes_number++;
-						else
-							segments.get(j).no_number++;
-					}
-				}
-				if (found==0){
-					segments.add(temp);
-					if ((boolean) DataStructureSingleton.getInstance1()[i][DataStructureSingleton.getInstance1()[0].length-1].getData())
-						segments.get(segments.size()-1).yes_number++;
+	public void extractSegments(Attribute[][] attributes){
+		for (int i=0; i<attributes.length ; i++){
+			boolean isFound=false;
+			Subset initial = new Subset(attributes[i][this.index]);
+			for( Subset s : subsets) {
+				if (s.attr.getData().equals( initial.attr.getData() ) ){
+					isFound = true;
+					s.numOccurrences++;
+					if ((boolean) attributes[i][attributes[0].length-1].getData()) // target column is always boolean
+						s.yesCount++;
 					else
-						segments.get(segments.size()-1).no_number++;
+						s.noCount++;
 				}
-				else{
-					found=0;
-				}
+			}
+			if ( !isFound ){
+				Subset temp=new Subset(attributes[i][index]);
+				temp.numOccurrences++;
+				if ((boolean) attributes[i][attributes[0].length-1].getData())
+					temp.yesCount++;
+				else
+					temp.noCount++;
+				subsets.add(temp);
 			}
 		}
 	}
 	
-	public void calculateInformationGain(double entropyParent, double range){	
+	public void calculateInformationGain(Attribute[][] attributes, double entropyParent, int range) {	
 		
 		
-		if (this.attribute instanceof Categorical || this.attribute instanceof Binary){
-			extractSegments();
+		if (this.attribute instanceof Categorical || this.attribute instanceof Binary ){
+			extractSegments(attributes);
 			
 			/**uncomment if you want to see how many yes and no are there for each segment **/
 //			System.out.println("column: "+index+" segments size: "+segments.size());
@@ -78,14 +70,24 @@ public class Node {
 //			}
 //			System.out.print('\n');
 			
-			double entropyTemp=0;
-			for (int i=0; i<segments.size(); i++){
-				double all_for_current_segment=segments.get(i).yes_number+ segments.get(i).no_number;
-				entropyTemp+= (all_for_current_segment/range)*Entropy.calcEntropy(segments.get(i).yes_number, segments.get(i).no_number);
+			double entropySegment = 0;
+			for ( Subset s : subsets) {
+				double segmentSize = s.yesCount + s.noCount;
+				s.entropy = ((double)segmentSize/(double)range)*Entropy.calcEntropy(s.yesCount, s.noCount);
+				entropySegment += s.entropy;
 //				System.out.println(all_for_current_segment);
 			}
 			
-			this.informationGain=entropyParent-entropyTemp;
+			double maxSubsetEntropy = 0;
+			for ( Subset s : subsets) {
+				if ( s.entropy > maxSubsetEntropy )
+				{
+					maxSubsetEntropy = s.entropy;
+					maximal          = s;
+				}
+			}
+			
+			this.informationGain = entropyParent - entropySegment;
 			
 		}
 		/**TODO: sort and split on numerical data**/
--- trunk/ml-cs-bonn/src/ml_ex1/Train.java
+++ trunk/ml-cs-bonn/src/ml_ex1/Train.java
@@ -6,7 +6,6 @@ public class Train {
 	public static void main(String[] args)  {
 		
 		TdidtAlgo algo = new TdidtAlgo();
-
 		
 //		FileReader.printReadDataArray(DataStructureSingleton.getInstance1());
 //		FileReader.printReadDataArrayList(DataStructureSingleton.getInstance());
