ad0f476dacbb132a1b908ed47bcedf2671df086b
marekpolewczyk <marekpolewczyk@34124984-cd06-a1ee-f7bf-09feb149d9c3>
2014-11-05 02:36:07 +0000

    first step of information gain calculation, TODO: do it in a loop for next levels and add information gain calculation for numerical data

--- ml-cs-bonn/src/ml_ex1/Train.java
+++ ml-cs-bonn/src/ml_ex1/Train.java
@@ -8,35 +8,17 @@ import java.util.List;
 
 public class Train {
 	
-	//list of list of attributes to store the data read from file
-	
 	public static Tree tree;
 	
-	public static void main(String[] args) throws FileNotFoundException, IOException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	public static void main(String[] args)  {
 		
 		tree=new Tree();
-		tree.calculateTargetEntropy(DataStructureSingleton.getInstance1()); //calculate the entropy of the target which is used in other calculations
+		double entropy_target = tree.calculateTargetEntropy(DataStructureSingleton.getInstance1()); //calculate the entropy of the target which is used in other calculations
+		double range = DataStructureSingleton.getInstance1().length;
+		tree.go(entropy_target, range);
 		
-		Node temp;
-		Node nodeHighestEntropy=new Node(0, DataStructureSingleton.getPattern1()[0], DataStructureSingleton.getNodeNames()[0]); //ascribe values of first attribute to the temporary node
-		nodeHighestEntropy.calculateEntropy(); //calculate entropy for the first attribute
-		
-		//finding node with the highest entropy (examining for each attribute)- first step of algorithm
-		//the upper range is -1 because the last attribute is the target
-		for (int i=1; i<FileReader.pattern1.length-1; i ++){
-			temp=new Node(i, DataStructureSingleton.getPattern1()[i], DataStructureSingleton.getNodeNames()[i]); //ascribte subsequent node to the temporary variable
-			temp.calculateEntropy(); //calculate entropy for temporary node
-			
-			if (temp.entropy-nodeHighestEntropy.entropy>0){ //check if old one is greater than new one (looking for maximum entropy)
-				nodeHighestEntropy=temp; //if greater save node with maximum entropy to nodeHighestEntropy
-			}
-		}
-		
-	
-		tree.setRoot(nodeHighestEntropy); //setting root node for the one with the highest entropy (greedy algorithm)
-		
-		FileReader.printReadDataArray(DataStructureSingleton.getInstance1());
-		FileReader.printReadDataArrayList(DataStructureSingleton.getInstance());
+//		FileReader.printReadDataArray(DataStructureSingleton.getInstance1());
+//		FileReader.printReadDataArrayList(DataStructureSingleton.getInstance());
 
 	}
 }
