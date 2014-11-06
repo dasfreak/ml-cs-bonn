9d9abaf7d481cfb5075cbb05c2d0f00fe446b41d
elias3@gmail.com <elias3@gmail.com@34124984-cd06-a1ee-f7bf-09feb149d9c3>
2014-11-06 14:34:37 +0000

    - Added DisjointSets class to hold the splits for numerical categories

---
+++ ml-cs-bonn/src/ml_ex1/DisjointSets.java
@@ -0,0 +1,14 @@
+package ml_ex1;
+
+public class DisjointSets {
+	Subset set1;
+	Subset set2;
+	double mean;
+	
+	public DisjointSets( Subset set1, Subset set2, double mean )
+	{
+		this.set1 = set1;
+		this.set2 = set2;
+		this.mean = mean;
+	}
+}
