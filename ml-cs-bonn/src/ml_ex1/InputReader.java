3c952f939b9cf57cb17b0532f296bf8ded4d1de8
elias3@gmail.com <elias3@gmail.com@34124984-cd06-a1ee-f7bf-09feb149d9c3>
2014-11-07 10:03:07 +0000

    more cleanup and renaming

--- ml-cs-bonn/src/ml_ex1/InputReader.java
+++ ml-cs-bonn/src/ml_ex1/InputReader.java
@@ -1,23 +1,27 @@
 package ml_ex1;
 
-import java.util.List;
-
-public class DataStructureSingleton {
+public class InputReader {
 	//private static FileReader originalData;
 	private static FileReader trainingSet;
 	private static FileReader testSet;
 	
-	protected DataStructureSingleton() {
+	protected InputReader() {
 	      // Exists only to defeat instantiation.
 	}
 	
-	public static FileReader getInstance1() {
+	public static FileReader getInstance() {
 	      if( trainingSet == null) {
 	    	 // uncomment this to use whole training set
 	    	 //  trainingSet = new FileReader("data_exercise_1.csv");
 	    	  trainingSet = new FileReader("training_set.csv");
-	    	  testSet     = new FileReader("test_set.csv");
 	      }
 	      return trainingSet;
-	}	
+	}
+	
+	public static FileReader getTestSet() {
+	      if( testSet == null) {
+	    	  testSet = new FileReader ("test_set.csv");
+	      }
+	      return testSet;
+	}
 }
